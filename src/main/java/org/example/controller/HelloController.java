309ce614b224060d44982d9c94bcbdc01bea17cd
xz <xz-jasper@qq.com>
2025-12-03 22:47:19 +0800

    init

--- src/main/java/org/example/controller/HelloController.java
+++ src/main/java/org/example/controller/HelloController.java
@@ -1,2 +1,13 @@
-package org.example.controller;public class HelloController {
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot 4!";
+    }
 }
